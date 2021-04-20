3f16998ef7cd7e0a2da816e154b3455d7a84e757
Bunikhovski1 <79472947+Bunikhovski1@users.noreply.github.com>
2021-04-20 14:35:54 +0300

    Add files via upload

---
+++ Lab22.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+  const int a = 25;
+  const int b = 52;
+  const int c = -9;
+  float y;
+
+  y = ((5*a - b + pow(c, 2)) / (2+c)) + 1;
+
+  printf("Результат:\n\n");
+  printf("Відповідь: ");
+  printf("y = %.1f", y);
+  return 0;
+}
