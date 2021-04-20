43dcc29172882fdc88c16e89200fbbac574f393d
Bunikhovski1 <79472947+Bunikhovski1@users.noreply.github.com>
2021-04-20 14:38:15 +0300

    Add files via upload

---
+++ Lab23.c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+  float a, x, y, z;
+
+  printf("Задайте три цілих числа:\n");
+
+  printf("\nВведіть число x: ");
+  scanf("%f", &x);
+
+  printf("Введіть число y: ");
+  scanf("%f", &y);
+
+  printf("Введіть число z: ");
+  scanf("%f", &z);
+
+  //Обчислення
+  a = (1+y) * ((x + (y / (pow(x, 2) + 4))) / (expf(-x - 2) + (1 / (pow(x, 2) + 4))));
+
+  printf("\nРезультат: %.3f\n", a);
+
+  return 0;
+}
